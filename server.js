a09e526893e0895b45692f7eb5178e055829a026
Usman Ali <162821354+Usman-co@users.noreply.github.com>
2024-10-23 23:04:58 +0500

    "commit"

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path to your db.json file
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 8000, () => {
+server.listen(3000, () => {
   console.log('JSON Server is running');
 });
